d8757da3e9e3081b8eb4a4f1de9f0a84fcff1537
João Pedro <Devjoao@xz235.onmicrosoft.com>
2023-06-05 16:53:21 -0300

    Curso bradesco

---
+++ Cliente.py
@@ -0,0 +1,15 @@
+class Cliente:
+    def __init__(self, n, fone):
+
+        self._nome = n
+        self._telefone = fone
+
+
+# Método Get
+def get_nome(self):
+    return self._nome
+
+
+# Método Self
+def self_nome(self, nome):
+    self.nome = nome
---
+++ Main.py
@@ -0,0 +1,15 @@
+from Conta import Conta
+from Cliente import Cliente
+
+
+class Main:
+    pass
+
+
+print("Testando o projeto")
+
+
+c1 = Cliente("João", "114444-2222")
+conta = Conta(c1._nome, 6565, 0)
+
+print(conta.titular, "Numero:", conta.numero, "Seu Saldo:", conta.saldo)
