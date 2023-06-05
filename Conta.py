be8d83185cc8f6868e972655f4415d7666d5b931
Pandinha <DevJoao@xz235.onmicrosoft.com>
2023-06-05 20:29:00 -0300

    ultima aula

--- Conta.py
+++ Conta.py
@@ -1,16 +1,19 @@
 class Conta:
     def __init__(self, titular, numero, saldo):
-        self.saldo = 0
-        self.numero = numero
-        self.titular = titular
+        self._saldo = 0
+        self._numero = numero
+        self._titular = titular
 
-    @property
-    def saldo(self):
-        return self._saldo
-
-    @saldo.setter
-    def saldo(self, saldo):
-        if (saldo < 0):
-            print("O saldo não pode ser negativo!")
+    def saque(self, valor):
+        if(self._saldo >= valor):
+            self._saldo -= valor
+            print("Saque realizada com sucesso")
         else:
-            self._saldo = saldo
+            print("Saldo insuficiente")
+    
+    def deposita(self,valor):
+        self._saldo += valor
+        
+    def extrato(self):
+        print("Cliente: ",self._titular, " Saldo Atual: ",self._saldo)
+        
--- Main.py
+++ Main.py
@@ -12,4 +12,6 @@ print("Testando o projeto")
 c1 = Cliente("João", "114444-2222")
 conta = Conta(c1._nome, 6565, 0)
 
-print(conta.titular, "Numero:", conta.numero, "Seu Saldo:", conta.saldo)
+conta.deposita(1000)
+conta.saque(200)
+conta.extrato()
