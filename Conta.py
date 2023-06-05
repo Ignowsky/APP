e3cafe4cb9ff347b287239dbc6569e7d627ca838
Pandinha <DevJoao@xz235.onmicrosoft.com>
2023-06-05 20:33:25 -0300

    atualização

--- Conta.py
+++ Conta.py
@@ -1,19 +1,19 @@
 class Conta:
     def __init__(self, titular, numero, saldo):
-        self._saldo = 0
-        self._numero = numero
-        self._titular = titular
+        self.saldo = 0
+        self.numero = numero
+        self.titular = titular
 
     def saque(self, valor):
-        if(self._saldo >= valor):
-            self._saldo -= valor
+        if(self.saldo >= valor):
+            self.saldo -= valor
             print("Saque realizada com sucesso")
         else:
             print("Saldo insuficiente")
     
     def deposita(self,valor):
-        self._saldo += valor
+        self.saldo += valor
         
     def extrato(self):
-        print("Cliente: ",self._titular, " Saldo Atual: ",self._saldo)
+        print("Cliente: ",self.titular, " Saldo Atual: ",self.saldo)
         
